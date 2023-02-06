d7ecf6412dbbe5a331b5feffe05c53050ba934b1
BOILLOT DEVALLIERE Vincent <vboillot@hotmail.fr>
2023-02-06 09:29:44 +0100

    feat: First implementation Commit.java

--- src/fr/uca/jgit/model/Commit.java
+++ src/fr/uca/jgit/model/Commit.java
@@ -1,6 +1,13 @@
 package fr.uca.jgit.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.MessageDigest;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Commit implements JGitObject {
     private List<Commit> parents;
@@ -8,17 +15,83 @@ public class Commit implements JGitObject {
 
     @Override
     public String hash() {
-        return null;
+        StringBuilder hexString = new StringBuilder();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            for(Commit entry : this.parents){
+                byte[] messageDigest = md.digest(entry.hash().getBytes());
+                for (byte b : messageDigest) {
+                    hexString.append(String.format("%02x", b & 0xff));
+                }
+            }
+            byte[] messageDigest = md.digest(state.hash().getBytes());
+            for (byte b : messageDigest) {
+                hexString.append(String.format("%02x", b & 0xff));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hexString.toString();
     }
 
     /** Stores the corresponding object in .git directory (to file .git/logs/[hash]). **/
     @Override
-    public void store() {
+    public void store() { // How to get the message of the commit ?
+        try {
+            StringBuilder content = new StringBuilder();
+            File myObj = new File(".git/logs/"+ this.hash());
+            FileWriter myWriter = new FileWriter(".git/logs/"+ this.hash());
+
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            for(Commit c : parents){
+                content.append(c.hash()).append(";");
+            }
+            content.deleteCharAt(content.length()-1);
+            content.append("\n");
+            content.append(" ").append(java.time.LocalTime.now()).append("-").append(java.time.LocalDate.now()).append("\n");
+            // append commit message ?
+            content.append(state.hash());
+
+            myWriter.write(content.toString());
+            myWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
+
+
 
     /** Loads the commit corresponding to the given hash (from file .git/logs/[hash]). **/
     public static Commit loadCommit(String hash) {
-        return null;
+
+        Commit newCommit = new Commit();
+        try {
+            File myObj = new File(".git/logs/"+hash);
+            Scanner myReader = new Scanner(myObj);
+
+            String[] sParents = myReader.nextLine().split(";");
+            for(String s: sParents){
+                newCommit.parents.add(Commit.loadCommit(s));
+            }
+            String temp = "";
+            while(myReader.hasNextLine()){
+                temp = myReader.nextLine();
+            }
+            newCommit.state = Folder.loadFolder(temp);
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return newCommit;
     }
 
     /** Checkout the commit.
