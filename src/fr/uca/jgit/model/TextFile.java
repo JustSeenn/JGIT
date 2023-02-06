7fee67d737d13bf980bd921e3f51fb6eb1772db9
BOILLOT DEVALLIERE Vincent <vboillot@hotmail.fr>
2023-02-06 09:28:00 +0100

    fix: File is no more compress

--- src/fr/uca/jgit/model/TextFile.java
+++ src/fr/uca/jgit/model/TextFile.java
@@ -30,26 +30,18 @@ public class TextFile implements Node {
     public void store() {
         try {
             File myObj = new File(".git/object/"+ this.hash());
+            FileWriter myWriter = new FileWriter(".git/object/"+ this.hash());
+
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
                 System.out.println("File already exists.");
             }
+            myWriter.write(this.content); // compress it ?
+            myWriter.close();
+
         } catch (IOException e) {
             System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            GZIPOutputStream gzipOS = new GZIPOutputStream(baos);
-            gzipOS.write(this.content.getBytes());
-            gzipOS.close();
-            baos.close();
-            myWriter.write(baos.toString());
-            myWriter.close();
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -57,7 +49,6 @@ public class TextFile implements Node {
     /** Loads the text file corresponding to the given hash (from file .git/object/[hash]). **/
     public static TextFile loadFile(String hash) {
         StringBuilder content = new StringBuilder();
-
         try {
             File myObj = new File(".git/object/"+hash);
             Scanner myReader = new Scanner(myObj);
@@ -78,18 +69,19 @@ public class TextFile implements Node {
     @Override
     public void restore(String path) {
         try {
-            FileInputStream fis = new FileInputStream(".git/object"+ this.hash());
-            GZIPInputStream gzipIS = new GZIPInputStream(fis);
-            FileOutputStream fos = new FileOutputStream(path);
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = gzipIS.read(buffer)) != -1) {
-                fos.write(buffer, 0, len);
+            File myObj = new File(path);
+            FileWriter myWriter = new FileWriter(path);
+
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
             }
-            gzipIS.close();
-            fos.close();
-            fis.close();
+            myWriter.write(this.content);
+            myWriter.close();
+
         } catch (IOException e) {
+            System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
