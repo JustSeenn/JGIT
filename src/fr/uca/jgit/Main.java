eaff3dd78e39ad360ed4344bedd31bdc49fca925
hebabz <heba.bouzidi@etu.unice.fr>
2023-02-14 14:11:32 +0100

    Demo updated

--- src/fr/uca/jgit/Main.java
+++ src/fr/uca/jgit/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         try {
-            Folder.initJGit();
+            RepositoryController.initJGit();
         } catch (Exception e) {
             System.out.println(e.toString());
         }
