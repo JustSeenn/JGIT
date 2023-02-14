d89f139024381b18aa62c7b9cfa8841aecea9892
BOILLOT DEVALLIERE Vincent <vboillot@hotmail.fr>
2023-02-14 16:54:04 +0100

    fix: forgot file

---
+++ src/main/fr/uca/jgit/controller/RepositoryController.java
@@ -0,0 +1,90 @@
+package fr.uca.jgit.controller;
+
+import fr.uca.jgit.model.Commit;
+import fr.uca.jgit.model.Folder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class RepositoryController {
+    public static void commit(Commit c1){
+        c1.store();
+
+        Path filePath = Paths.get(".jgit", "HEAD");
+
+        StringBuilder content = new StringBuilder();
+        for (Commit c : c1.getParents()) {
+            content.append(c.hash()).append(";");
+        }
+        if(content.length() > 0)
+            content.deleteCharAt(content.length()-1);
+        content.append("\n");
+        content.append(LocalTime.now().toString()).append("-").append(LocalDate.now()).append("\n");
+        content.append(c1.hash());
+
+        try {
+            Files.write(filePath, content.toString().getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to file.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void initJGit() {
+        try {
+            if(Files.exists(Paths.get(".jgit"))){
+                System.out.println("Directories already exist!");
+                return;
+            }
+            Path newPath = Paths.get(".jgit");
+            Files.createDirectories(newPath);
+            newPath = Paths.get(".jgit", "object");
+            Files.createDirectories(newPath);
+            newPath = Paths.get(".jgit", "logs");
+            Files.createDirectories(newPath);
+            newPath = Paths.get(".jgit", "HEAD");
+            Files.createFile(newPath);
+            System.out.println("Directories are created!");
+        } catch (IOException e) {
+            System.err.println("Failed to create directories!" + e.getMessage());
+        }
+    }
+
+    public static void changeBranch(String hash) {
+        // Check if the branch exists
+        File branchFile = new File(Paths.get(".jgit", "logs", hash).toString());
+        if (!branchFile.exists()) {
+            System.out.println("Branch " + hash + " does not exist");
+            return ;
+        }
+
+        // Get the hash for the state of repo
+        String lastLine = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(branchFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lastLine = line;
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Failed to read the last line of the file.");
+            e.printStackTrace();
+        }
+
+        Commit commit = Commit.loadCommit(hash);
+        commit.setState(Folder.loadFolder(lastLine));
+        commit.checkout();
+
+    }
+}
