9979580c93c5cf811d9d2d9b262beab5ab794c5b
BOILLOT DEVALLIERE Vincent <vboillot@hotmail.fr>
2023-02-26 12:09:53 +0100

    feat: Update cucumber tests with WorkingDirectory

--- src/test/java/fr/uca/jgit/controller/RepositoryControllerStepdefs.java
+++ src/test/java/fr/uca/jgit/controller/RepositoryControllerStepdefs.java
@@ -1,15 +1,22 @@
 package fr.uca.jgit.controller;
 
 
+import fr.uca.jgit.Main;
+import fr.uca.jgit.command.Init;
+import fr.uca.jgit.command.Merge;
+import fr.uca.jgit.command.StateCommit;
+import fr.uca.jgit.model.WorkingDirectory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import fr.uca.jgit.controller.RepositoryController;
 import fr.uca.jgit.model.Commit;
 import fr.uca.jgit.model.Folder;
 import fr.uca.jgit.model.TextFile;
-import java.io.IOException;
+
+import javax.inject.Inject;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,108 +26,79 @@ public class RepositoryControllerStepdefs {
     Folder folder;
     Commit commit1;
     Commit commit2;
-
-    Commit result;
+    @Inject
+    WorkingDirectory wd;
 
 
 
     public RepositoryControllerStepdefs(){}
 
+    @Given("a repository .jgit")
+    public void a_repository_jgit() {
+        Init init = new Init();
+        init.execute(String.valueOf(Paths.get(String.valueOf(Path.of(System.getProperty("user.dir"))),"temp")));
+    }
     @Given("a file named file with the content {string}")
-    public void aFile(String content) {
+    public void a_file_named_file_with_the_content(String content) {
         file = new TextFile(content);
     }
 
+
     @Given("a folder named folder1")
-    public void aFolder() {
+    public void a_folder_named_folder1() {
         folder = new Folder();
     }
 
+    @Given("a working directory")
+    public void aWorkingDirectory() {
+        WorkingDirectory.getInstance().setPath(Paths.get("temp"));
 
-    @Given("a commit named commit1")
-    public void aCommit() {
-         commit1 = new Commit();
+    }
+
+    @Given("a commit named commit1 which is the current commit")
+    public void a_commit_named_commit1_which_is_the_current_commit() {
+        commit1 = new Commit();
+        folder.add("file", file.clone());
+        commit1.setState(folder.clone());
+
+        StateCommit st = new StateCommit();
+        WorkingDirectory.getInstance().setPath(Paths.get("temp"));
+        WorkingDirectory.getInstance().setCurrentCommit(commit1);
+        st.execute("commit1");
     }
 
     @Given("a commit named commit2")
-    public void anotherCommit() {
-         commit2 = new Commit();
+    public void a_commit_named_commit2() {
+        commit2 = new Commit();
     }
 
-    @When("f1 wants to be merge with f2 and f1 and f2 have the same content")
-    public void mergef1SameAsf2() throws IOException {
-        folder.add("file", file.clone());
-        commit1.setState(folder.clone());
-
-        folder.add("file", file.clone());
-        commit2.setState(folder.clone());
-        result = RepositoryController.merge(commit1, commit2);
-    }
-
-    @Then("the result of the merge has the content {string}")
-    public void resultMerge(String content) {
-        Folder newState = result.getState();
-        // print all the children of newState as a list
-        TextFile newFile = (TextFile) newState.getChildren().get("file");
-        newFile.setContent(newFile.getContent().replace("\n", " "));
-        System.out.println(newFile.getContent() + " " + content);
-        assertEquals(newFile.getContent(), content);
-    }
-
-    @Then("the result of the merge is a conflict with the content {string}")
-    public void resultMergeConflict(String content) {
-        Folder newState = result.getState();
-        TextFile newFile = (TextFile) newState.getChildren().get("file.cl");
-        newFile.setContent(newFile.getContent().replace("\n", " "));
-        System.out.println(newFile.getContent() + " " + content);
-        assertEquals(newFile.getContent(), content);
-    }
-
-
-    @When("f1 wants to be merge with f2  and f1 modified the file by {string}")
-    public void f1WantsToBeMergeWithF2AndF1ModifiedTheFileByAddingTheToBetweenHelloAndWorld(String newContent) throws IOException {
-        folder.add("file", file.clone());
-        commit1.setState(folder.clone());
-
-        file.setContent(newContent);
-        folder.add("file", file.clone());
+    @When("the content of commit2 is {string}")
+    public void the_content_of_commit2_is(String content) {
+        file.setContent(content);
+        ((TextFile) folder.getChildren().get("file")).setContent(content);
         commit2.setState(folder.clone());
 
-        result = RepositoryController.merge(commit2, commit1);
+        StateCommit st = new StateCommit();
+        WorkingDirectory.getInstance().setPath(Paths.get("temp"));
+        WorkingDirectory.getInstance().setCurrentCommit(commit2);
+        st.execute("commit2");
+
     }
 
-    @When("f1 wants to be merge with f2 and f1 delete the first line")
-    public void f1WantsToBeMergeWithF2AndF1DeleteTheFirstLine() throws IOException {
-        folder.add("file", file.clone());
-        commit1.setState(folder.clone());
-
-        file.setContent(file.getContent().replace("Hello World\n", ""));
-        folder.add("file", file.clone());
-        commit2.setState(folder.clone());
-
-        result = RepositoryController.merge(commit1, commit2);
+    @When("I do the command git merge {string}")
+    public void i_do_the_command_git_merge(String arg0) {
+        Merge m = new Merge();
+        WorkingDirectory.getInstance().setPath(Paths.get("temp"));
+        m.execute(arg0);
     }
 
-    @When("f1 wants to be merge with f2  and f1 modified the file by {string} and f2 modified the file by {string}")
-    public void f1WantsToBeMergeWithF2AndF1ModifiedTheFileByAddingTheToBetweenHelloAndWorldAndF2ModifiedTheFileByAddingTheToBetweenHelloAndWorld(String newContent1, String newContent2) throws IOException {
-        folder.add("file", file.clone());
-        commit1.setState(folder.clone());
-
-        file.setContent(newContent1);
-        folder.add("file", file.clone());
-        commit2.setState(folder.clone());
-
-        result = RepositoryController.merge(commit2, commit1);
-
-        file.setContent(newContent2);
-        folder.add("file", file.clone());
-        commit2.setState(folder.clone());
-
-        result = RepositoryController.merge(commit2, commit1);
+    @Then("The result of the merge has the content {string}")
+    public void the_result_of_the_merge_has_the_content(String content) {
+        WorkingDirectory.getInstance().setPath(Paths.get("temp"));
+        String headHash = RepositoryController.getHeadHash();
+        Commit c = Commit.loadCommit(headHash);
+        TextFile f = (TextFile) c.getState().getChildren().get("file");
+        System.out.println(f.getContent() + " " + content);
+        assertEquals(f.getContent(), content);
     }
-
-
-
-
-
 }
